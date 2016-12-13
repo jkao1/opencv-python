7c91389189fedd48f2423599d81c4928c93d396a
jkao1 <jkao1@stuy.edu>
2016-12-12 19:01:55 -0500

    saved

--- opencv-test.py
+++ opencv-test.py
@@ -4,40 +4,26 @@ sys.path.append('/usr/local/lib/python2.7/site-packages')
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
+cap = cv2.VideoCapture(0)
 
-img1 = cv2.imread("lindan.jpg")
-img2 = cv2.imread("leechongwei.jpg")
+while (cap.isOpened()):
+    _, frame = cap.read() # we need value cuz of function return, but _ is useless
+    hsv = cv2.cvtColor(frame, 0)# hue saturation value, another way to deifne colors
+# 0 represents cv2.COLOR_BGR2GRAY
+    lower_yellow = np.array([0,0,0], dtype=np.uint8)
+    upper_yellow = np.array([255,255,255], dtype=np.uint8)
 
-# shape contains [numPixelsUp/Down, numPixelsLeft/Right, and channels]
-rows,cols,channels = img2.shape
-# we want to overlap img2 on img1, so we define our roi as img1[dimensions of img2]
-roi = img1[0:rows,0:cols] 
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    res = cv2.bitwise_and(frame, frame, mask=mask) # where in the frame is it in the frame and mask (in the range = 1 (black), not in range = 0 (white))
 
-# convert img2 so later we can read pixel colors as 1 value from 0-255
-img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame',frame)
+    cv2.imshow('frame',mask)
+    cv2.imshow('frame',res)
 
-# inverse binary threshold: 2 outcomes --> white or black
-# if >= 240, will be converted to black (black = background)
-# if less than 240, will be converted to white (white = foreground)
-# mask is the black part
-# WHAT IS RET
-ret,mask = cv2.threshold(img2gray, 50, 255, cv2.THRESH_BINARY_INV)
+    key = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
 
-# makes blacked out area invisible
-mask_inv = cv2.bitwise_not(mask)
-
-# 
-img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
-#img2_foreground 
-img2_fg = cv2.bitwise_and(img2, img2, mask=mask)
-
-dst = cv2.add(img1_bg, img2_fg)
-img1[0:rows,0:cols] = dst
-
-cv2.imshow('res',img2_fg)
-cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
+cap.release()
