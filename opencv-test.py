26cd6bb7cae4c8fafdddf1af3ec778de2b3f86ec
jkao1 <jkao1@stuy.edu>
2016-12-12 19:42:27 -0500

    3

--- opencv-test.py
+++ opencv-test.py
@@ -5,25 +5,49 @@ sys.path.append('/usr/local/lib/python2.7/site-packages')
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('../Github/opencv-python/lindan.mp4')
 
-while (cap.isOpened()):
-    _, frame = cap.read() # we need value cuz of function return, but _ is useless
-    hsv = cv2.cvtColor(frame, 0)# hue saturation value, another way to deifne colors
-# 0 represents cv2.COLOR_BGR2GRAY
-    lower_yellow = np.array([0,0,0], dtype=np.uint8)
-    upper_yellow = np.array([255,255,255], dtype=np.uint8)
+while(cap.isOpened()):
+    ret, frame = cap.read()
 
-    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-    res = cv2.bitwise_and(frame, frame, mask=mask) # where in the frame is it in the frame and mask (in the range = 1 (black), not in range = 0 (white))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+"""
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    boundaries = [
+	([17, 15, 100], [50, 56, 200]),
+	([86, 31, 4], [220, 88, 50]),
+	([25, 146, 190], [62, 174, 250]),
+	([103, 86, 65], [145, 133, 128])
+    ]
+
+    lower = np.array(boundaries[1][0])
+    upper = np.array(boundaries[1][1])
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower, upper)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
 
     cv2.imshow('frame',frame)
-    cv2.imshow('frame',mask)
-    cv2.imshow('frame',res)
-
-    key = cv2.waitKey(5) & 0xFF
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
 
 cv2.destroyAllWindows()
-cap.release()
+"""
